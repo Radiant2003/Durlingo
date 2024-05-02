59a3a23c706fed046014519534cea10c2c8795b3
Давид Мандрыкин <davidmandrykin@MacBook-Air-David.local>
2024-05-03 00:17:50 +0300

    initial

---
+++ constants.ts
@@ -0,0 +1,24 @@
+export const POINTS_TO_REFILL = 10;
+
+export const quests = [
+    {
+        title: "earn 20 XP",
+        value: 20,
+    },
+    {
+        title: "earn 50 XP",
+        value: 50,
+    },
+    {
+        title: "earn 100 XP",
+        value: 100,
+    },
+    {
+        title: "earn 500 XP",
+        value: 500,
+    },
+    {
+        title: "earn 1000 XP",
+        value: 1000,
+    },
+];
